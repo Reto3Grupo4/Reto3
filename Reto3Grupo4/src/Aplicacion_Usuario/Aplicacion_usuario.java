8d4abd00035ea7f0dd88ae6cdd09c7156dd0e43f
Reto3Grupo4 <in1dm3-v@elorrieta.int>
2026-04-28 21:21:49 +0200

    Subida de Aplicacion de usuario

--- Reto3Grupo4/src/Aplicacion_Usuario/Aplicacion_usuario.java
+++ Reto3Grupo4/src/Aplicacion_Usuario/Aplicacion_usuario.java
@@ -16,6 +16,11 @@ import javax.swing.border.TitledBorder;
 import java.awt.CardLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.border.EtchedBorder;
+import java.awt.Color;
+import javax.swing.JTextArea;
 
 public class Aplicacion_usuario extends JFrame implements ActionListener{
 
@@ -60,7 +65,7 @@ public class Aplicacion_usuario extends JFrame implements ActionListener{
 	public Aplicacion_usuario() {
 		setTitle("Aplicacion Usuario");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 579);
+		setBounds(100, 100, 530, 579);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -205,9 +210,88 @@ public class Aplicacion_usuario extends JFrame implements ActionListener{
 		Ventana_Registro.add(btnComprarPremium);
 		
 		JPanel Ventana_Menu = new JPanel();
-		Ventana_Menu.setBorder(new TitledBorder(null, "Menu", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		Ventana_Menu.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Ventana Menu", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		contentPane.add(Ventana_Menu, "name_622576064905800");
 		Ventana_Menu.setLayout(null);
+		
+		JButton btnAtras_2 = new JButton("Atras");
+		btnAtras_2.setBounds(10, 24, 89, 23);
+		Ventana_Menu.add(btnAtras_2);
+		
+		JButton btnPerfil = new JButton("Perfil");
+		btnPerfil.setBounds(301, 24, 89, 23);
+		Ventana_Menu.add(btnPerfil);
+		
+		JLabel lblEscoger = new JLabel("ESCOGE");
+		lblEscoger.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblEscoger.setHorizontalAlignment(SwingConstants.CENTER);
+		lblEscoger.setBounds(167, 28, 57, 14);
+		Ventana_Menu.add(lblEscoger);
+		
+		JButton btnDescubrirMusica = new JButton("Descubrir musica");
+		btnDescubrirMusica.setBounds(130, 88, 126, 23);
+		Ventana_Menu.add(btnDescubrirMusica);
+		
+		JButton btnDescubrirPodcast = new JButton("Descubrir podcast");
+		btnDescubrirPodcast.setBounds(130, 122, 126, 23);
+		Ventana_Menu.add(btnDescubrirPodcast);
+		
+		JButton btnMisPlaylist = new JButton("Mis Playlist");
+		btnMisPlaylist.setBounds(130, 156, 126, 23);
+		Ventana_Menu.add(btnMisPlaylist);
+		
+		JPanel Ventana_Artistas = new JPanel();
+		contentPane.add(Ventana_Artistas, "name_625363922644100");
+		Ventana_Artistas.setLayout(null);
+		
+		JButton btnAtras_3 = new JButton("Atras");
+		btnAtras_3.setBounds(10, 11, 89, 23);
+		Ventana_Artistas.add(btnAtras_3);
+		
+		JButton btnPerfil_1 = new JButton("Perfil");
+		btnPerfil_1.setBounds(314, 11, 89, 23);
+		Ventana_Artistas.add(btnPerfil_1);
+		
+		JLabel lblListaArtistas = new JLabel("Lista de artistas");
+		lblListaArtistas.setHorizontalAlignment(SwingConstants.CENTER);
+		lblListaArtistas.setBounds(142, 25, 118, 14);
+		Ventana_Artistas.add(lblListaArtistas);
+		
+		JTextArea txtAreaListaArtistas = new JTextArea();
+		txtAreaListaArtistas.setBounds(71, 61, 255, 193);
+		Ventana_Artistas.add(txtAreaListaArtistas);
+		
+		JPanel Ventana_Artista = new JPanel();
+		contentPane.add(Ventana_Artista, "name_625778799462100");
+		Ventana_Artista.setLayout(null);
+		
+		JButton btnAtras_3_1 = new JButton("Atras");
+		btnAtras_3_1.setBounds(10, 11, 89, 23);
+		Ventana_Artista.add(btnAtras_3_1);
+		
+		JButton btnPerfil_2 = new JButton("Perfil");
+		btnPerfil_2.setBounds(325, 11, 89, 23);
+		Ventana_Artista.add(btnPerfil_2);
+		
+		JPanel pnlListaDisco = new JPanel();
+		pnlListaDisco.setBorder(new TitledBorder(null, "Lista Disco", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		pnlListaDisco.setBounds(10, 55, 227, 464);
+		Ventana_Artista.add(pnlListaDisco);
+		pnlListaDisco.setLayout(null);
+		
+		JTextArea txtAreaDiscos = new JTextArea();
+		txtAreaDiscos.setBounds(10, 21, 207, 166);
+		pnlListaDisco.add(txtAreaDiscos);
+		
+		JPanel pnlInformacion = new JPanel();
+		pnlInformacion.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Informacion", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		pnlInformacion.setBounds(247, 55, 247, 464);
+		Ventana_Artista.add(pnlInformacion);
+		pnlInformacion.setLayout(null);
+		
+		JTextArea txtAreaInformacion = new JTextArea();
+		txtAreaInformacion.setBounds(10, 21, 207, 166);
+		pnlInformacion.add(txtAreaInformacion);
 
 	}
 
