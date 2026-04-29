0d708004c827a54ea686c691db478337c408328a
Reto3Grupo4 <jonimanol.gurruchagaga@elorrieta-errekamari.com>
2026-04-29 17:41:45 +0200

    subida de los roles y los usuarios ( ADMIN hecho, empleado a mitad y cliente 1 y 2 sin hacer

---
+++ Documents/Reto3/SQL/Usuarios_Permisos.sql
@@ -0,0 +1,34 @@
+use destruyespotify;
+
+/*Creación del rol de administrador*/
+CREATE ROLE IF NOT EXISTS 'DBADMIN';
+GRANT ALL PRIVILEGES ON destruyespotify.* TO 'DBADMIN';
+
+/*Creación del Usuario */
+CREATE USER IF NOT EXISTS 
+'AdminSpoty'@'localhost' IDENTIFIED BY '1234';
+
+/*Asignación del rol creado anteriormente*/
+GRANT 'DBADMIN' TO 'AdminSpoty'@'localhost';
+SET DEFAULT ROLE 'DBADMIN' TO 'AdminSpoty'@'localhost';
+show grants for 'AdminSpoty'@'localhost';
+
+FLUSH PRIVILEGES;
+
+CREATE ROLE IF NOT EXISTS 'EmpleSpoty';
+GRANT select ON destruyespotify.audio TO 'EmpleSpoty';
+GRANT select ON destruyespotify.artista TO 'EmpleSpoty';
+GRANT select ON destruyespotify.podcast TO 'EmpleSpoty';
+GRANT select ON destruyespotify.playlist TO 'EmpleSpoty';
+GRANT select ON destruyespotify.cancion TO 'EmpleSpoty';
+GRANT select ON destruyespotify.album TO 'EmpleSpoty';
+GRANT select ON destruyespotify.cancion TO 'EmpleSpoty';
+GRANT select ON destruyespotify.playlist_canciones TO 'EmpleSpoty';
+GRANT select ON destruyespotify.podcaster TO 'EmpleSpoty';
+
+GRANT UPDATE ON destruyespotify.
+
+
+
+CREATE USER IF NOT EXISTS 
+'EmpleSpoty'@'localhost' IDENTIFIED BY '1234';
