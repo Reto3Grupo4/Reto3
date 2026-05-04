aeb576dbe7e36e8e478a4c0c47cbde6a269b1906
andergil-cmd <in1dm3-v@elorrieta.int>
2026-05-04 18:40:08 +0200

    Pagina web modificada(Falta el xml, xsd y xslt)

---
+++ Documents/Reto3Grupo4/Lenguaje_Marcas/javascript.js
@@ -0,0 +1,5 @@
+function validarLogin(){
+    const nombreUsuario = document.getElementById('usuario').value;
+    const contrasena = document.getElementById('contrasena').value;
+    if(nombreUsuario)
+}
